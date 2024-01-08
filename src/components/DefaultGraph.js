e5eb018ea5f51fd66ed89ea81bf2cccb97cc3b97
Pramod Hegde <you@example.compramod.hegde@neutrinos.co>
2024-01-08 16:02:03 +0530

    working drag and drop

---
+++ src/components/DefaultGraph.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react";
+
+import Graph from "graphology";
+import {
+  useSigma,
+  useRegisterEvents,
+  useLoadGraph,
+  useSetSettings,
+} from "@react-sigma/core";
+import { useLayoutCircular } from "@react-sigma/layout-circular";
+
+import { useSeedRandom } from "./useSeedRandom";
+
+export const GraphDefault = ({ order, probability }) => {
+  const { faker, randomColor } = useSeedRandom();
+  const sigma = useSigma();
+  const { assign: assignCircular } = useLayoutCircular();
+  const registerEvents = useRegisterEvents();
+  const loadGraph = useLoadGraph();
+  const setSettings = useSetSettings();
+  const [hoveredNode, setHoveredNode] = useState(null);
+
+  useEffect(() => {
+    // Create the graph
+    const graph = new Graph();
+    for (let i = 0; i < order; i++) {
+      graph.addNode(i, {
+        label: faker.name.fullName(),
+        size: faker.datatype.number({ min: 4, max: 20, precision: 1 }),
+        color: randomColor(),
+        x: 0,
+        y: 0,
+      });
+    }
+    for (let i = 0; i < order; i++) {
+      for (let j = i + 1; j < order; j++) {
+        if (Math.random() < probability) graph.addDirectedEdge(i, j);
+        if (Math.random() < probability) graph.addDirectedEdge(j, i);
+      }
+    }
+
+    loadGraph(graph);
+    assignCircular();
+
+    // Register the events
+    registerEvents({
+      enterNode: (event) => setHoveredNode(event.node),
+      leaveNode: () => setHoveredNode(null),
+    });
+  }, [
+    assignCircular,
+    loadGraph,
+    registerEvents,
+    faker.datatype,
+    faker.name,
+    randomColor,
+    order,
+    probability,
+  ]);
+
+  useEffect(() => {
+    setSettings({
+      nodeReducer: (node, data) => {
+        const graph = sigma.getGraph();
+        const newData = { ...data, highlighted: data.highlighted || false };
+
+        if (hoveredNode) {
+          if (
+            node === hoveredNode ||
+            graph.neighbors(hoveredNode).includes(node)
+          ) {
+            newData.highlighted = true;
+          } else {
+            newData.color = "#E2E2E2";
+            newData.highlighted = false;
+          }
+        }
+        return newData;
+      },
+      edgeReducer: (edge, data) => {
+        const graph = sigma.getGraph();
+        const newData = { ...data, hidden: false };
+
+        if (hoveredNode && !graph.extremities(edge).includes(hoveredNode)) {
+          newData.hidden = true;
+        }
+        return newData;
+      },
+    });
+  }, [hoveredNode, setSettings, sigma]);
+
+  return null;
+};
--- src/components/SigmaGraph.js
+++ src/components/SigmaGraph.js
@@ -1,10 +1,81 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "@react-sigma/core/lib/react-sigma.min.css";
 import { MultiDirectedGraph } from "graphology";
-import { SigmaContainer, useRegisterEvents } from "@react-sigma/core";
+import { SigmaContainer, useRegisterEvents, useSigma } from "@react-sigma/core";
 import _ from "lodash";
 
 const SigmaGraph = ({ graphData }) => {
+  const GraphEvents = () => {
+    const registerEvents = useRegisterEvents();
+    const sigma = useSigma();
+    const [draggedNode, setDraggedNode] = useState(null);
+
+    useEffect(() => {
+      // Register the events
+      registerEvents({
+        downNode: (e) => {
+          console.log("downNode");
+          setDraggedNode(e.node);
+          sigma.getGraph().setNodeAttribute(e.node, "highlighted", true);
+        },
+        mouseup: (e) => {
+          console.log("mouseup");
+          if (draggedNode) {
+            setDraggedNode(null);
+            sigma.getGraph().removeNodeAttribute(draggedNode, "highlighted");
+          }
+        },
+        mousedown: (e) => {
+          console.log("mousedown");
+          // Disable the autoscale at the first down interaction
+          if (!sigma.getCustomBBox()) sigma.setCustomBBox(sigma.getBBox());
+        },
+        mousemove: (e) => {
+          console.log("mousemove");
+          if (draggedNode) {
+            // Get new position of node
+            const pos = sigma.viewportToGraph(e);
+            sigma.getGraph().setNodeAttribute(draggedNode, "x", pos.x);
+            sigma.getGraph().setNodeAttribute(draggedNode, "y", pos.y);
+
+            // Prevent sigma from moving camera:
+            e.preventSigmaDefault();
+            e.original.preventDefault();
+            e.original.stopPropagation();
+          }
+        },
+        touchup: (e) => {
+          console.log("touchup");
+          if (draggedNode) {
+            setDraggedNode(null);
+            sigma.getGraph().removeNodeAttribute(draggedNode, "highlighted");
+          }
+        },
+        touchdown: (e) => {
+          console.log("touchdown");
+          // Disable the autoscale at the first down interaction
+          if (!sigma.getCustomBBox()) sigma.setCustomBBox(sigma.getBBox());
+        },
+        touchmove: (e) => {
+          console.log("touchmove");
+          if (draggedNode) {
+            // Get new position of node
+            const pos = sigma.viewportToGraph(e);
+            sigma.getGraph().setNodeAttribute(draggedNode, "x", pos.x);
+            sigma.getGraph().setNodeAttribute(draggedNode, "y", pos.y);
+
+            // Prevent sigma from moving camera:
+            e.preventSigmaDefault();
+            e.original.preventDefault();
+            e.original.stopPropagation();
+          }
+        },
+      });
+    }, [registerEvents, sigma, draggedNode]);
+
+    return null;
+  };
+
   const graph = new MultiDirectedGraph();
   _.forEach(_.get(graphData, "nodes", []), (node) => {
     graph.addNode(_.get(node, "id"), {
@@ -16,7 +87,6 @@ const SigmaGraph = ({ graphData }) => {
     });
   });
   _.forEach(_.get(graphData, "edges", []), (edge) => {
-    console.log(edge, "ed");
     graph.addEdge(
       _.get(edge, "source"),
       _.get(edge, "target", { label: _.get(edge, "id") })
@@ -24,9 +94,17 @@ const SigmaGraph = ({ graphData }) => {
   });
 
   return (
-    <SigmaContainer style={{ height: "500px" }} graph={graph}></SigmaContainer>
+    <>
+      <SigmaContainer
+        style={{ height: "500px" }}
+        graph={graph}
+        order={100}
+        probability={0.1}
+      >
+        <GraphEvents />
+      </SigmaContainer>
+    </>
   );
 };
 
 export default SigmaGraph;
-
